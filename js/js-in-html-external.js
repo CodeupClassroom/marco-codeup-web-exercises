94397f28f06c7c75de155cfe8b97b7eeb4f70167
Douglas Hirsh <douglas@codeup.com>
2021-02-02 11:24:48 -0600

    feat: go over JS interactive dialogs

--- js/js-in-html-external.js
+++ js/js-in-html-external.js
@@ -3,3 +3,15 @@ console.log('Hello from an external JS file.');
 
 var name = "Douglas";
 console.log("Hello, " + name);
+
+//Alert
+alert("Welcome to my site!");
+
+//Confirm
+var confirmLeave = confirm("Are you sure you want to leave? O.O");
+console.log("confirmLeave " + confirmLeave);
+
+//Prompt
+var favoriteColor = prompt("What is your favorite color?");
+console.log("Your favorite color is " + favoriteColor);
+
