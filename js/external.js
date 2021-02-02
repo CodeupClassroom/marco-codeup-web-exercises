b0a6e35a10eb6922328351d1ca78a2588fa2de37
Samuel C Moore <samuel.c.moore1221@gmail.com>
2021-02-02 16:31:17 -0600

    feat: added comments to exernal.js for easier student digestion

--- js/external.js
+++ js/external.js
@@ -1,17 +1,22 @@
 console.log("Hello from external JavaScript");
 
+// here we can use the alert function to give the user a winder on top of their browser
 alert("Welcome to my Website!");
 
+// here we prompt the user for a string with the prompt function
 var UsersFavoriteColor = prompt("What is your favorite color?");
 
+// we can concatenate stings withe the user input to make a dynamic message
 alert("Great, " + UsersFavoriteColor + " is my favorite color too!!");
 
+//we can use the parseInt function to convert a string to a int (or also called a number)
 var littleMermaid = parseInt(prompt("How many days have you had the Little Mermaid?"));
 
 var brotherBear = parseInt(prompt("How many days have you had Brother Bear?"));
 
 var hercules = parseInt(prompt("How many days have you had Hercules?"));
 
+// we can add all the values the user has given to see a total of how much is owed
 var totalOwed = (littleMermaid + brotherBear + hercules) * 3;
 
 alert("You owe BlockBuster $" + totalOwed);
