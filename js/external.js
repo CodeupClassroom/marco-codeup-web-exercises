f4848e3dcc22c1d52f93692f136ff4db4bdb015b
Samuel C Moore <samuel.c.moore1221@gmail.com>
2021-02-03 10:00:59 -0600

    feat: Finished the JavaScript and html exercise

--- js/external.js
+++ js/external.js
@@ -1,36 +1,62 @@
-console.log("Hello from external JavaScript");
+// console.log("Hello from external JavaScript");
+//
+// // here we can use the alert function to give the user a winder on top of their browser
+// alert("Welcome to my Website!");
+//
+// // here we prompt the user for a string with the prompt function
+// var UsersFavoriteColor = prompt("What is your favorite color?");
+//
+// // we can concatenate stings withe the user input to make a dynamic message
+// alert("Great, " + UsersFavoriteColor + " is my favorite color too!!");
+//
+// //we can use the parseInt function to convert a string to a int (or also called a number)
+// var littleMermaid = parseInt(prompt("How many days have you had the Little Mermaid?"));
+//
+// var brotherBear = parseInt(prompt("How many days have you had Brother Bear?"));
+//
+// var hercules = parseInt(prompt("How many days have you had Hercules?"));
+//
+// // we can add all the values the user has given to see a total of how much is owed
+// var totalOwed = (littleMermaid + brotherBear + hercules) * 3;
+//
+// alert("You owe BlockBuster $" + totalOwed);
+//
+// var hoursWorkedForGoogle = Number(prompt("How many hours did you work for Google?"));
+//
+// var hoursWorkedForAmazon = Number(prompt("How many hours did you work for Amazon?"));
+//
+// var hoursWorkedForFacebook = Number(prompt("How many hours did you work for Facebook?"));
+//
+// var totalFromGoogle = hoursWorkedForGoogle * 400;
+//
+// var totalFromAmazon = hoursWorkedForAmazon * 380;
+//
+// var totalFromFacebook = hoursWorkedForFacebook * 350;
+//
+// alert("You have made a total of $" + (totalFromGoogle + totalFromAmazon + totalFromFacebook) + " this week!");
+//
+// var classIsFull = confirm("Is the class you are trying to join full?");
+//
+// var hasScheduleConflicts = confirm("Do you have a class at 2:30 already?");
+//
+// alert("You can join the class is a " + (!classIsFull && !hasScheduleConflicts) + " statement.");
 
-// here we can use the alert function to give the user a winder on top of their browser
-alert("Welcome to my Website!");
+var isOfferValid = confirm("Is the offer still valid?");
 
-// here we prompt the user for a string with the prompt function
-var UsersFavoriteColor = prompt("What is your favorite color?");
+var hasMoreThanTwoItems = parseInt(prompt("How many items do you have in your cart?")) >= 2;
 
-// we can concatenate stings withe the user input to make a dynamic message
-alert("Great, " + UsersFavoriteColor + " is my favorite color too!!");
+var isPremiumMember = confirm("Are you a premium member?");
 
-//we can use the parseInt function to convert a string to a int (or also called a number)
-var littleMermaid = parseInt(prompt("How many days have you had the Little Mermaid?"));
+var canUserUseOffer = (hasMoreThanTwoItems || isPremiumMember) && isOfferValid;
 
-var brotherBear = parseInt(prompt("How many days have you had Brother Bear?"));
+alert("You can use this offer is a " + canUserUseOffer + " statement.");
 
-var hercules = parseInt(prompt("How many days have you had Hercules?"));
+// You can also do this
 
-// we can add all the values the user has given to see a total of how much is owed
-var totalOwed = (littleMermaid + brotherBear + hercules) * 3;
+// var hasEnoughItems = hasMoreThanTwoItems || isPremiumMember;
+//
+// var canUserUseOffer = hasEnoughItems && isOfferValid;
 
-alert("You owe BlockBuster $" + totalOwed);
 
-var hoursWorkedForGoogle = Number(prompt("How many hours did you work for Google?"));
 
-var hoursWorkedForAmazon = Number(prompt("How many hours did you work for Amazon?"));
 
-var hoursWorkedForFacebook = Number(prompt("How many hours did you work for Facebook?"));
-
-var totalFromGoogle = hoursWorkedForGoogle * 400;
-
-var totalFromAmazon = hoursWorkedForAmazon * 380;
-
-var totalFromFacebook = hoursWorkedForFacebook * 350;
-
-alert("You have made a total of $" + (totalFromGoogle + totalFromAmazon + totalFromFacebook) + " this week!");
