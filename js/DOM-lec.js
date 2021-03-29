1445544bbdab6d7d0caa0884a707e606d91edeaf
kennethhowell <howell.kenneth.a@gmail.com>
2021-03-29 10:13:21 -0500

    complete: first part of lecture, breakout into activity

---
+++ js/DOM-lec.js
@@ -0,0 +1,13 @@
+// var topHeaderElement = document.getElementById("top-header");
+//
+// console.log(topHeaderElement);
+
+//How else can we locate? We can check out CLASS and TAG as well
+
+// var currentOffers = document.getElementsByClassName("special-offer")
+//
+// console.log(currentOffers);
+
+// var listItems = document.getElementsByTagName("li")
+//
+// console.log(listItems);
