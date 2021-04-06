35ce4ba4688dd3fb1b478cf78a8a61cb1199a5e5
Douglas Hirsh <douglas@codeup.com>
2021-04-06 07:48:02 -0500

    feat: jQuery Intro Lecture

---
+++ js/jquery-intro-lec.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    
+   console.log("Fully loaded and ready to go.");
+   console.log("Hello, " + personName);
+});
+
+var personName = "Douglas";
+console.log("Hello, jQuery!");
