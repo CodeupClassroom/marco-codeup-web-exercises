ef0033af3f30d8d20c3e38d850f84b57db4edae0
Douglas Hirsh <douglas@codeup.com>
2021-04-19 11:26:05 -0500

    feat: add promises lecture code

--- js/promises-lec.js
+++ js/promises-lec.js
@@ -5,14 +5,28 @@
  *        USING FETCH WITH PROMISES
  ******************************************** */
 
-var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon');
+// var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokexmon');
 // return of fetch request is a Promise object
 
 //TODO: console log the results of the above Promise. What is the result?
 
+//console.log(pokemonAPI);
+
 //TODO: add a method that runs if the Promise resolves successfully
 
+// pokemonAPI.then((response)=> {
+//     response
+//         .json()
+//         .then((jsonResponseData)=> {
+//             console.log(jsonResponseData)
+//             //fetch
+//                 //then
+//                     //then
+//         });
+// });
+
 //TODO: add a method that runs if the Promise fails
+//pokemonAPI.catch(reason => console.log(reason));
 
 //TODO: In the callback function of the .then method, parse the response into JSON
 
@@ -25,11 +39,33 @@ var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon');
 //  the first returned response.
 //    Taking a look at this second return, what are the results? What is the shape of our data?
 
+// var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon')
+//     .then((response)=>{return response.json()})
+//     .then((jsonData)=>{return jsonData.results})
+//     .then((results)=>results.forEach(result=>console.log(result.name)));
+
 // TODO: Finally, chain another .then method that would log all of the name properties of the
 //  returned pokemon.
 // BONUS: Is there a way for us to clean up our code?
 
 // Let's try working with the Star Wars API!
+let starWarsAPI = fetch("https://swapi.dev/api/films")
+    //  Parse the response into json
+    .then((response)=>{return response.json()})
+    // access the results property from the json Object
+    .then((jsonData)=>{
+        console.log(jsonData);
+        return jsonData.results;
+    })
+    // iterate over the results array, and access title and id properties of each film
+    .then((results)=>{
+        results.forEach((film)=>console.log(`${film.title} ${film.episode_id}`))
+    })
+    // This runs if my fetch request fails. Try changing films to xfilms in the url and look at the error that gets passed. It isn't what you think it would be.
+    .catch(error => console.log('Error getting films.', error));
+
+// Notice how this log runs before the we log the results.
+console.log('starWarsAPI Promise', starWarsAPI);
 
 // TODO: Using Promises, make a fetch request to the Star Wars API
 
@@ -38,4 +74,13 @@ var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon');
 // TODO: chain another method that iterates through the results array and console logs the names
 
 // TODO: Demonstrate Promise.all and Promise.race
-
+// let starWarsAPI = fetch("https://swapi.dev/api/films")
+// var pokemonAPI = fetch('https://pokeapi.co/api/v2/pokemon')
+// // Promise.all([starWarsAPI, pokemonAPI])
+// //     .then((responses)=>{
+// //       console.log(responses);
+// //     })
+//
+// Promise.race([starWarsAPI,pokemonAPI]).then((response)=>{
+//     console.log(response);
+// })
